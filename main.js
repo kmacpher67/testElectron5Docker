4aed94ce9caba6e58fa14b53c126021fbd10161b
kenmac <ken.macpherson@philips.com>
2019-10-02 14:28:03 -0400

    main test uncommitted issues no clue what they were

--- main.js
+++ main.js
@@ -6,16 +6,15 @@ const { netLog } = require('electron')
 const fs = require('fs');
 var createdWindow=false;
 var printedPDF=false;
+var loggedIn=false;
+var loginRetry=0;
 
 console.log("run with export ELECTRON_ENABLE_LOGGING=true for renderer logs = " + process.env.ELECTRON_ENABLE_LOGGING);
 
 process.enablePromiseAPIs=true;
 
-setTimeout(function(){ 
-    console.log("LOGS: 2222 console.log electron.app isReady 120ms=" + app.isReady());
-}, 120);
-
 process.send = process.send || function () {};
+
 setTimeout(function(){ 
   process.send('LOGS: process.send electron.app isReady= ' + app.isReady());
   if (app.isReady() && !createdWindow) {
@@ -30,11 +29,11 @@ setTimeout(function(){
     console.log("NOT READY!!! ");
     process.send('LOGS: createMainWindow(); is NOT NOT ready');
   }
-}, 120);
+}, 20);
 
 function createMainWindow() {
   console.log("main.js - createMainWindow() before.")
-  netLog.startLogging('/home/kenmac/dev-ui/testElectron5/net-log-'+new Date().toTimeString());
+  netLog.startLogging('/home/kenmac/dev-ui/testElectron5/net-log-'+new Date().getTime());
  if (app.isReady) {
   process.send('LOGS: ele.main.js: startng win = new electron.BrowserWindow');
   var win = new electron.BrowserWindow(
@@ -46,23 +45,15 @@ function createMainWindow() {
   win.openDevTools();
 
       var d = new Date().getTime(); //'+d.toString()+'
+      
       // and load the index.html of the app.
       //win.loadFile('index.html')
       // win.loadURL('http://kenmacpherson.com');
-      //win.loadURL('http://localhost:4200/');
-      win.loadURL('http://localhost:8000/');
+      win.loadURL('http://localhost:4200/');
+      //win.loadURL('http://localhost:8000/');
 
       win.webContents.on('did-finish-load', () => {
         process.send('did-finish-load: ele.main.js:= win. printedPDF');
-            // Use default printing options
-            setTimeout(function(){
-                console.log("did-finish-load ====");
-                if (!printedPDF) {
-                  console.log("PRINTING PDF per did-finish-load  ====");
-                  printedPDF=true;
-                  printPDF("did-finish-load");
-                }  
-              }, 200);
 
             // not loaded? 
             var jsLogInjector = "(function () {  console.log('inside win.webContents.on( did-finish-load renderer injected code: jsLogInjector'); " +
@@ -70,7 +61,11 @@ function createMainWindow() {
             // "  ipc.send('logs', 'hey this is FROM TEST INJECTED javascript code sending log renderer'); " + 
             "})();";
             win.webContents.executeJavaScript(jsLogInjector);
-      })
+
+            if (!loggedIn) {
+              loginApp();
+            }
+      });
 
       // win.webContents.on('pr-ready-to-print-event', (event, level, message, line, sourceId) => {
       //   console.log("pr-ready-to-print-event: occurred from main.js event=" + event);
@@ -89,19 +84,40 @@ function createMainWindow() {
         // console.log("console-message: occurred from main.js sourceId=" + sourceId);
         process.send("console-message: " + message);
         if (!printedPDF && message.includes("pr-ready-to-print-event")) {
-          console.log("PRINTING PDF per ready to print event!!!");
+          console.log("PRINTING PDF!!! per ready to print event!!!");
           printedPDF=true;
           printPDF('pr-ready-to-print-event');
         }
+
+        if (message.includes('User not authenticated')) {
+          loggedIn=false;
+          setTimeout(function(){ loginApp(); }, 120);
+        }
       });
 
-      win.webContents.on('ipc-message', (event, channel, args) => {
-        console.log("ipc-message: occurred from main.js event=" + JSON.stringify(event));
-        console.log("ipc-message: occurred from main.js channel=" + channel);
-        console.log("ipc-message: occurred from main.js args=" + args);
-        process.send("ipc-message: event=" + event);
-      });
+      // win.webContents.on('ipc-message', (event, channel, args) => {
+      //   console.log("ipc-message: occurred from main.js event=" + JSON.stringify(event));
+      //   console.log("ipc-message: occurred from main.js channel=" + channel);
+      //   console.log("ipc-message: occurred from main.js args=" + args);
+      //   process.send("ipc-message: event=" + event);
+      // });
 
+    }
+
+    function loginApp() {
+      // INFO: prToggleService: Cannot fetch toggles. User not authenticated
+      loggedIn=true;
+      if (loginRetry++ < 5) {
+          //First jsInjection will get us logged into the app
+          console.log("_config.locale =" + _config.locale );
+          var jsInjection = "try { var injector = angular.element(document.body).injector();" +
+          "injector.get('prAuthAuthService').createSession(" + JSON.stringify(_config.session) + ");" +
+          "injector.get('prLanguageService').changeLocale('" + _config.locale + "')" +
+          "} catch (ex) { var ipc = require('electron').ipcRenderer; ipc.send('error', ex.message); } ";
+
+          win.webContents.executeJavaScript(jsInjection);
+          process.send('WE ARE NOW LOGGED INTO THE APP'); 
+      }
     }
 
     function printPDF(filenamePrePackcage) {
@@ -110,7 +126,7 @@ function createMainWindow() {
         fs.writeFile(filenamePrePackcage+'-'+d.toString()+'.pdf', data, (error) => {
           if (error) throw error
           console.log('Write PDF successfully.' + filenamePrePackcage);
-          setTimeout(function(){ app.quit(); },19500);
+          setTimeout(function(){ app.quit(); },99500);
         })
       });
     }
@@ -118,68 +134,42 @@ function createMainWindow() {
     process.send('LOGS: jsLogInjector = executeJavaScript' );
     process.send("LOGS: before injector code 1 logs render" );
 
-    var jsLogInjector = "(function () {  console.log('STARTING 11111 AFTER on did-finsih  renderer injected code: jsLogInjector'); " +
+    var jsLogInjector = "(function () {  console.log('injecting console log into render AFTER on did-finsih  renderer injected code: jsLogInjector'); " +
     // "  var ipc = require('electron').ipcRenderer; " +
     // "  ipc.send('logs', 'hey this is FROM TEST INJECTED javascript code sending log renderer'); " + 
     "})();";
     win.webContents.executeJavaScript(jsLogInjector);
 
-
-    // var jsLogInjector = "(function () {  var app = angular.module('appRoot',[]);  " +
-    // "    var injector = angular.element(document.body).injector(); " +
-    // "  var ipc = require('electron').ipcRenderer; " +
-    // "  ipc.send('logs', 'hey this is FROM TEST INJECTED javascript code sending log renderer'); " + 
-    // "})();";
-    // win.webContents.executeJavaScript(jsLogInjector);
-
-    // var jsLogInjector = "(function () { var injector = angular.element(document.body).injector(); " +
-    // "  var ipc = require('electron').ipcRenderer; " +
-    // "  var $log = injector.get('$log'); " +
-    // "  var log = $log.debug; " +
-    // "  $log.debug = function () { " +
-    // "    ipc.send('logs', Array.prototype.slice.call(arguments)); log.apply(this, Array.prototype.slice.call(arguments));" +
-    // "  }; " +
-    // "})();";
-    // win.webContents.executeJavaScript(jsLogInjector);
-
-    // var jsLogInjectorError = "(function () { var injector = angular.element(document.body).injector(); " +
-    // "  var ipc = require('electron').ipcRenderer; " +
-    // "  var $log = injector.get('$log'); " +
-    // "  var log = $log.error; " +
-    // "  $log.error = function () { " +
-    // "    ipc.send('error', Array.prototype.slice.call(arguments)); log.apply(this, Array.prototype.slice.call(arguments));" +
-    // "  }; " +
-    // "})();";
-    // win.webContents.executeJavaScript(jsLogInjectorError);
-
 };// of createMainWindow function!!
 
 app.on('ready', async function () {
   filename = process.argv.pop();  //get the filename with UUID
+  process.send('CONFIG');
 
   if (!createdWindow) {
     createdWindow=true;
     process.send('logs: createMainWindow(); is ready from app.on(ready, function() ');
-    createMainWindow();
+    setTimeout(function(){ createMainWindow(); }, 90);
   }
   console.log("this is the createdWinow empty log")
   process.send("process.send createdWindow end:");
 });
 
-ipcMain.on('error', function(ev, error) {
-  process.send('ERROR: from renderer-' + error);
-});
-
-ipcMain.on('logs', function(ev, logs) {
-  process.send('LOGS: from renderer-'+ logs);
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+      app.quit();
+  }
 });
 
 process.on('message', function(config) {
   _config = config;
   console.log("message, function(config) occurred");
   process.send("message:");
-  createMainWindow();
+  if (!createdWindow) {
+    createMainWindow();
+  }
 });
+
 
 console.log = function (message) {
   process.send("LOGS: " + message);
--- test.js
+++ test.js
@@ -5,24 +5,37 @@ var proc = require('child_process');
 var filename = 'testpdfprint.pdf';
 var child;
 var sandbox='--no-sandbox'; //'--no-sandbox';
-var logNet='--log-net-log=/home/kenmac/dev-ui/testElectron5/net-log'+new Date().getMilliseconds();
+var logNet='--log-net-log=/home/kenmac/dev-ui/testElectron5/net-log'+new Date().getTime()
 var enableLogging='--enable-logging';
-
+var config={};
 var exec = require('child_process').exec;
 
-	//exec('ps -ef', function(err, stdout, stderr) {
-	  // stdout is a string containing the output of the command.
-	//  console.log("=====   BEFORE stdout="+stdout);
-	//});
+
+    // copy and paste a http headers into this variable. 
+    config.url="http://localhost:4200/#/patient/cbec38f7-b8f7-4661-a295-68f1a82b26df/summary?activeReportFamily=2&timeframe=0&daysSelection=30&days=30&startDate=Tue%20Mar%2026%202019%2000:00:00%20GMT-0400%20(Eastern%20Daylight%20Time)&endDate=Wed%20Apr%2024%202019%2000:00:00%20GMT-0400%20(Eastern%20Daylight%20Time)";
+    config.debug=false;  // this value if true doesn't close the visual window (the show will always be true)
+    config.stod=0;
+    config.documentId= "req.body.documentId"+new Date().toISOString();
+    config.transactionId="trans_id" + new Date().getMilliseconds(); //this is the GUID from the ui-server
+    // auth token put into login block (copy)
+    //auth_token: {"userId":"1e46a995-c4b0-414a-9f01-1d2c3be22c54","uniqueId":"d56ca88f-a154-4bfa-814a-59eb91ad32e0","hash":"pW2GSSKEmAr3ggYv8lFh9DZ2m+iJtmJgrKiTUTlFs0c/biaoS7b837ALB7OZVg2CwmJtlbl/VSxmLgVimvrjsw==","timeToLive":21600}
+
+    config.token={"userId":"1e46a995-c4b0-414a-9f01-1d2c3be22c54","uniqueId":"d56ca88f-a154-4bfa-814a-59eb91ad32e0","hash":"pW2GSSKEmAr3ggYv8lFh9DZ2m+iJtmJgrKiTUTlFs0c/biaoS7b837ALB7OZVg2CwmJtlbl/VSxmLgVimvrjsw==","timeToLive":21600}
+    // outer wrapper of authtoken copy paste into here
+    config.session = {"session": { "responseCode": 1, "token": { "userId": "1e46a995-c4b0-414a-9f01-1d2c3be22c54", "uniqueId": "d56ca88f-a154-4bfa-814a-59eb91ad32e0", "hash": "pW2GSSKEmAr3ggYv8lFh9DZ2m+iJtmJgrKiTUTlFs0c/biaoS7b837ALB7OZVg2CwmJtlbl/VSxmLgVimvrjsw==", "timeToLive": 21600 }, "userPermissions": ["Toggle_F2942_NightOne", "ViewCustomUsage", "ManageCustomFields", "AddPatient", "ManageCareProgram", "ViewCareProgram", "ViewUserSleepReports", "Toggle_US16865_NewTrilogyWorker", "ManagePatientRules", "Toggle_F678_EnableSubOrgInheritsFromTransformation", "ViewAdminTasks", "Toggle_F4575_CIS_Get_Accessories", "AssignDevice", "SearchPatients", "TemporaryPatientAccess", "ManageOrganizationSettings", "Toggle_F2261", "Toggle_F2535_SDM_PAMS_As_SubCat", "Toggle_F4327_PatientV2_KES_Producer", "Toggle_F2286_Trilogy_waveform", "Toggle_F2320_Payers_Migration", "EditUser", "Toggle_F2925_CustomizeCareProgram", "ViewCompanyPremiumFeatures", "Toggle_Japan-Standard", "ManagePayers", "Toggle_F2320_OrgMergeComponent", "Toggle_Trilogy_Reprocess", "ManageTherapyReportByBlowerTime", "ViewPayers", "ConfigureOrg", "ManageCareTeamLocationAutoAssign", "Toggle_US14576_OLH_2WayClienCertificateAuthentication", "SearchOrgs", "ViewUserFirstLastName", "CustomPatientListView", "ManageReportTemplatesTrilogy", "Toggle_F2320_TherapyTemplates_Migration", "ViewOrganizationSettings", "ResetUsersPasswords", "ViewUser", "Toggle_F2320_ReminderGroups_Migration", "CreatePrescription", "Toggle_F929_EnableTopOrgCountryLinkageTransformation", "ViewTherapyData", "SignBusinessAgreements", "ViewTasks", "Toggle_F405_OrgMergeSplit", "CreateAppInvitation", "Toggle_US18886_Move_PamsAutoAssign_To_AuthEntities", "Toggle_F2257", "Toggle_TrilogySummaryReport", "CreatePatientManualTask", "Toggle_US16810_TrilogyDataHandingLegacy", "ManageTherapyReportTemplates", "UpdateSearchOrgs", "SelectAdminLandingPage", "Toggle_F2513_ExtendedPatientSearch_Migration", "Toggle_F2745", "ViewEntityInfo", "ChangeMyPassword", "Toggle_F2804_US26595", "Toggle_F2513_NhsNumberCategoryPropagation", "ManagePatientViewTemplates", "Toggle_TextSearchContains", "Toggle_US24332_DoNotUseVaultForKafka", "Toggle_F886_careteam", "ManageTrilogyCareProgram", "ViewCompanyInfo", "ViewReminders", "UpdateSearchUsers", "UploadTherapyData", "FeatureEnablement", "Toggle_F4359_ExternalServiceRefactor", "Toggle_F2320_CustomFields_Migration", "Toggle_F2928_CpDurationNextCp", "Toggle_F2320_AvailableAccessories_Migration", "Toggle_TrilogyWithoutEvo_RxChangedRule", "Toggle_F888_Password_Reset", "ManageSDCardTrilogy", "ViewCompanyPremiumFeatureHistory", "ViewOrganizationTypes", "ManageCompany", "ManagePatientOrg", "AddUser", "Toggle_F2924", "ViewPrescription", "SelectClinicalLandingPage", "Toggle_F2665_EnablePatientCareTeamGroupTransformation", "DeleteTherapyData", "ManageTherapyReportTemplatesForTopOrg", "ManageEquipmentTrilogy", "AccessSapphire", "ManageRulesTrilogy", "Toggle_VentRawDataPersist", "EditPatient", "ManageSettings", "SearchUsers", "ViewTrilogyCareProgram", "ManageEquipment", "ManageRules", "Toggle_F2320_EntitySettingsTransformation", "ViewTrilogyReports", "ViewUserContactInfo", "ManageCustomUsage", "ViewCustomFields", "Toggle_F4590_OnePageReportTemplate", "ManageReminders", "ViewPatient", "ViewEquipment", "ViewTherapyReportTemplates", "Toggle_US20436_EnableTransformation", "ManageExternalAuthorizations", "ViewCompany"], "userLogin": "chimayclin", "userFirstName": "Chimay", "userLastName": "Clinician", "userEmailAddress": null, "userTopOrgId": "81844546-4aaa-4fb2-8eb9-05ca040bd2f6", "passwordMinLength": 8 } }
+    config.language="en";
+    config.locale="en-us";
+    config.parameters={"executedDate":"2019-08-22T17:18:26.874Z","patientUuid":"cbec38f7-b8f7-4661-a295-68f1a82b26df","orgId":"81844546-4aaa-4fb2-8eb9-05ca040bd2f6","userName":"Chimay Clinician","context":"device-summary","cache":{"cacheName":"REPORT-EXECUTOR-CACHE","cache":{"keys":["proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/therapy-summary-stats/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&serialNumbers=TV1190711A1","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/daily-device-usage/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/daily-device-usage/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/therapy-summary-stats/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&serialNumbers=TV1190711A1","proxy/SAPPHIREGATEWAY-V1-SERVER/vent/therapies/patients/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/vent/therapies/patients/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/vent/therapies/patients/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&stod=0","proxy/SAPPHIREGATEWAY-V1-SERVER/vent/therapies/patients/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&stod=0","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/therapy-summary-stats/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&serialNumbers=TV1190711A1","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/daily-device-usage/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/daily-device-usage/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24","proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/therapy-summary-stats/cbec38f7-b8f7-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&serialNumbers=TV1190711A1"]}}        };
+    config.save=true;  //always upload test the uploader to saved documents
+    // had to remove the escaped body codes backslash data still works this way, so there is basically no CACHE
+    config.cache={"name":"REPORT-EXECUTOR-CACHE","data":{"proxy/SAPPHIREGATEWAY-V1-SERVER/devicesummary/therapy-summary-stats/cbec38f7-b8f71-4661-a295-68f1a82b26df?start=2019-03-26&end=2019-04-24&serialNumbers=TV1190711A1":[200, "OK"]}}; 
 
 	console.log("=====   before child = proc.spawn(");
     //Start electron child process.  Pass in config as string and open IPC channel for sending messages
     child = proc.spawn(
         electron,
         [sandbox, logNet, enableLogging, 'main.js', filename],
-            {
-				ELECTRON_ENABLE_LOGGING:1,   
-				stdio: [null, null, null, 'ipc']}
+            {stdio: [null, null, null, 'ipc']}
      );
 
     console.log("=====   main.js stdout after before ps 2 function =============== \n");
@@ -66,9 +79,8 @@ var exec = require('child_process').exec;
     onEvent('close', child);
 	onEvent('exit', child);
 
-	function onEvent (event, child, fn) {
+	function onEvent (event, child) {
 		child.on(event, data => console.log(`PID::${child.pid} EVENT::${event.toUpperCase()} DATA::${data}`));
-		fn && fn();
 	}
 
 	console.log("== test.jsCallToMain: bottom of page");
